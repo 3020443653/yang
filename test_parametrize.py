b4ed06877584dc290c323eb72f90ef6e0ff7e3fc
3020443653 <3020443653@qq.com>
2021-04-16 14:26:48 +0800

    second commit

---
+++ test_parametrize.py
@@ -0,0 +1,18 @@
+import pytest
+def add_fuction(a,b):
+    return a+b
+#参数化，参数加别名，ids
+@pytest.mark.parametrize("a,b ,expected",
+                         [(3,5,8),
+                          (-1,-2,-3),
+                          (100000,100000,200000),
+                          ],ids=["int","minus","bigint"])
+def test_add(a,b,expected):
+    assert  add_fuction(a,b) == expected
+
+#参数可以组合
+@pytest.mark.parametrize("a",[0,1,20000])
+@pytest.mark.parametrize("b",[-2,10000,0.4])
+def test_foo(a,b):
+    print("测试数据组合：a->%s,b->%s"%(a,b))
+
